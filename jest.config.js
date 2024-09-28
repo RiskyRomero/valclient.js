28f2a573d417976c30231d9bd6757168a8534440
Ryumin <riskyromero@proton.me>
2024-09-28 21:26:08 +0800

    refactor: updated obsolete code

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint @typescript-eslint/no-var-requires: "off" */
+/* eslint @typescript-eslint/no-require-imports: "off" */
 // jest.config.js
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { pathsToModuleNameMapper } = require("ts-jest");
 const { compilerOptions } = require("./tsconfig.json");
 
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
